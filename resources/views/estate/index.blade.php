685fa2b9015f4d4f78bad53eb898c4ad878c856c
moon <kwarnkham.moon@gmail.com>
2018-12-18 23:00:09 +0630

    fix table

--- resources/views/estate/index.blade.php
+++ resources/views/estate/index.blade.php
@@ -1,48 +1,50 @@
 @extends('layout.master')
 
 @section('content')
-<div class="table-responsive">
-    <table class="table">
-        <thead>
-            <tr>
-                <th scope="col">#</th>
-                <th scope="col">Address</th>
-                <th scope="col">Road</th>
-                <th scope="col">Area</th>
-                <th scope="col">Type</th>
-                <th scope="col">Township</th>
-                <th scope="col">Price</th>
-                <th scope="col">Note</th>
-                <th scope="col">Deal</th>
-                <th scope="col">Picture</th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php $i=1 ?>
-            @foreach($estates as $estate)
-            <?php
-				if($estate->status == 1 ) { 
-					$color="bg-success"; 
-				} else { 
-					$color="bg-secondary"; 
-				}
-			?>
-            <tr class={{$color}}>
-                <th scope="row"><a class='border p-1 rounded border-warning bg-light' href="/estate/{{ $estate->id }}/edit"
-                        class="text-dark" style="text-decoration: none">{{$i }}</a></th>
-                <td>{{$estate->address}}</td>
-                <td>{{$estate->road}}</td>
-                <td>{{$estate->area}}</td>
-                <td>{{$estate->type->name}}</td>
-                <td>{{$estate->township->name}}</td>
-                <td>{{$estate->price}}</td>
-                <td>{{$estate->note}}</td>
-                <td>{{$estate->deal}}</td>
-                <td class="bg-light"><a href="/estate/{{ $estate->id }}/show" class="border border-success p-2 rounded">More</a></td>
-            </tr>
-            <?php $i++ ?>
-            @endforeach
-        </tbody>
-    </table>
+<div class="row">
+    <div class="col-12 rounded">
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Address</th>
+                    <th scope="col">Road</th>
+                    <th scope="col">Area</th>
+                    <th scope="col">Type</th>
+                    <th scope="col">Township</th>
+                    <th scope="col">Price</th>
+                    <th scope="col">Note</th>
+                    <th scope="col">Deal</th>
+                    <th scope="col">Picture</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php $i=1 ?>
+                @foreach($estates as $estate)
+                <?php
+                    if($estate->status == 1 ) { 
+                        $color="bg-success"; 
+                    } else { 
+                        $color="bg-danger"; 
+                    }
+                ?>
+                <tr class={{$color}}>
+                    <th scope="row"><a class='border p-1 rounded border-warning bg-light' href="/estate/{{ $estate->id }}/edit"
+                            class="text-dark" style="text-decoration: none">{{$i }}</a></th>
+                    <td>{{$estate->address}}</td>
+                    <td>{{$estate->road}}</td>
+                    <td>{{$estate->area}}</td>
+                    <td>{{$estate->type->name}}</td>
+                    <td>{{$estate->township->name}}</td>
+                    <td>{{$estate->price}}</td>
+                    <td>{{$estate->note}}</td>
+                    <td>{{$estate->deal}}</td>
+                    <td><a href="/estate/{{ $estate->id }}/show" class="border border-success p-1 rounded bg-light" style="text-decoration: none;">More</a></td>
+                </tr>
+                <?php $i++ ?>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
 </div>
 @endsection
