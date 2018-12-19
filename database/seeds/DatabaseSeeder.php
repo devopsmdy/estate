31464f1df3691a24bf14e9948da98f9a4e5cf486
moon <kwarnkham.moon@gmail.com>
2018-12-19 15:33:08 +0630

    test data generation

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $this->call(UsersTableSeeder::class);
+        $this->call(TownshipsTableSeeder::class);
+        $this->call(TypesTableSeeder::class);
+        $this->call(EstatesTableSeeder::class);
     }
 }
