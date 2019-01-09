794ee9b47f642ec6876c5560f6b71344bd3a91a4
moon <kwarnkham.moon@gmail.com>
2019-01-09 22:17:17 +0630

    back to list now now

--- resources/views/estate/index.blade.php
+++ resources/views/estate/index.blade.php
@@ -3,36 +3,49 @@
 @section('content')
 <div class="row">
     <div class="col-12 rounded">
-
-        <?php $i=1 ?>
-        @foreach($estates as $estate)
-        <?php
-            if($estate->status == 1 ) { 
-                $color="bg-light"; 
-            } else { 
-                $color="bg-secondary"; 
-            }
-        ?>
-
-        <!-- Card -->
-        <div class="card col-12 {{$color}} mb-3">
-            <div class="card-body">
-                <h5 class="card-title">Address: {{$estate->address}}</h5>
-                <h6 class="card-subtitle mb-2 text-muted">Road: {{$estate->road}}</h6>
-                <p class="card-text">Area: {{$estate->area}}</p>
-                <p class="card-text">Type: {{$estate->type->name}}</p>
-                <p class="card-text">Township: {{$estate->township->name}}</p>
-                <p class="card-text">Price: {{$estate->price}}</p>
-                <p class="card-text">Note: {{$estate->note}}</p>
-                <p class="card-text">Deal: {{$estate->deal}}</p>
-            </div>
-            <div class="card-footer">
-                <a href="/estate/{{ $estate->id }}/edit" class="card-link">Edit</a>
-                <a href="/estate/{{ $estate->id }}/show" class="card-link float-right">More</a>
-            </div>
-        </div>
-        <?php $i++ ?>
-        @endforeach
+        <table class="table table-sm">
+            <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Address</th>
+                    <th scope="col">Road</th>
+                    <th scope="col">Area</th>
+                    <th scope="col">Type</th>
+                    <th scope="col">Township</th>
+                    <th scope="col">Price</th>
+                    <th scope="col">Note</th>
+                    <th scope="col">Deal</th>
+                    <th scope="col">Picture</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php $i=1 ?>
+                @foreach($estates as $estate)
+                <?php
+                    if($estate->status == 1 ) { 
+                        $color="bg-success"; 
+                    } else { 
+                        $color="bg-secondary"; 
+                    }
+                ?>
+                <tr class={{$color}}>
+                    <th scope="row"><a class='border p-1 rounded border-warning bg-light' href="/estate/{{ $estate->id }}/edit"
+                            class="text-dark" style="text-decoration: none">{{$i }}</a></th>
+                    <td>{{$estate->address}}</td>
+                    <td>{{$estate->road}}</td>
+                    <td>{{$estate->area}}</td>
+                    <td>{{$estate->type->name}}</td>
+                    <td>{{$estate->township->name}}</td>
+                    <td>{{$estate->price}}</td>
+                    <td>{{$estate->note}}</td>
+                    <td>{{$estate->deal}}</td>
+                    <td><a href="/estate/{{ $estate->id }}/show" class="border border-success p-1 rounded bg-light" style="text-decoration: none;">More</a></td>
+                </tr>
+                <?php $i++ ?>
+                @endforeach
+            </tbody>
+        </table>
     </div>
+    <div class="mx-auto">{{ $estates->links() }}</div>
 </div>
 @endsection
