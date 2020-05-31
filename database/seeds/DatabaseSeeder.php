780d87a98e2dd77cc420f4e5cd3b87d8e592f2f3
kwarnkham <kwarnkham.moon@gmail.com>
2020-05-31 13:00:31 +0630

    schdule backup db

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,14 +11,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-<<<<<<< HEAD
-        // $this->call(UsersTableSeeder::class);
-        $this->call(EstateTableSeeder::class);
-=======
         $this->call(UsersTableSeeder::class);
         $this->call(TownshipsTableSeeder::class);
         $this->call(TypesTableSeeder::class);
         $this->call(EstatesTableSeeder::class);
->>>>>>> f0c73b1264be82e0a798d78798239336e6058297
     }
 }
